86d571a2c217c2fa8c4e569919244de14a7b6b63
SyedaIlmaAli <160448031+SyedaIlmaAli@users.noreply.github.com>
2024-05-27 03:15:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+// Day 04
+// Question 10 : It was about comments
+// Question 11
+let friendName = ["Ilma", "Mehak", "Asma"];
+for (let i = 0; i < friendName.length; i++) {
+    console.log(friendName[i]);
+}
+;
+// Question 12
+for (let i of friendName) {
+    console.log(`${i} how are you`);
+}
---
+++ index.ts
@@ -0,0 +1,17 @@
+// Day 04
+
+// Question 10 : It was about comments
+
+// Question 11
+
+let friendName : string[] = ["Ilma", "Mehak", "Asma"];
+for(let i = 0; i < friendName.length; i++){
+    console.log(friendName[i])
+};
+
+// Question 12
+
+for(let i of friendName){
+    console.log(`${i} how are you`)
+}
+
